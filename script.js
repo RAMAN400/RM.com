000aae20779c06b8db1884d9592552d293bac6b4
Raman <159559277+RAMAN400@users.noreply.github.com>
2024-03-06 22:13:07 +0545

    Update script.js

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ $(document).ready(function(){
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Designer", "Web Developer"],
+        strings: ["Programmer", "Developer", "Designer", "Web Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
